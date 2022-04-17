e6fd3326dda5c5cdb608693262f97ba32c32ede1
Lawrence Yee <lawrenceyee521@lawrences-mbp.lan>
2022-04-17 18:15:23 -0400

    temp home page

--- blky-bnz-frontend/src/components/Home.js
+++ blky-bnz-frontend/src/components/Home.js
@@ -1,5 +1,27 @@
-function Home(props) {
-  return <div class="Home">Home</div>;
+import { useState, useEffect } from "react";
+import portrait from "../Images/WallPaper.jpeg";
+
+function Home() {
+  const text = ["Hello ", "goodBye ", "take care "];
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    return setWord(text[1]);
+  }, []);
+
+  return (
+    <div class="home">
+      {/* <img className="homeCover" src={portrait}></img> */}
+      <div className="titleWrap">
+        <div className="title">
+          <h1>
+            <span>Health</span> is a state of body. <span>Welness</span> is a
+            state of being.
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
