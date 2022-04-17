ca16df4d0ad34b82dfdd607cdc59ef75196e6388
Nasim Akbor <nasimakbor@Nasims-Air.fios-router.home>
2022-04-17 00:35:54 -0400

    Added user functionality for Food list, started on organ list, made databases editable, connected images to OrganData box

--- blky-bnz-frontend/src/OrganData.js
+++ blky-bnz-frontend/src/OrganData.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { updateOrgan } from "./services/reqfunctions.js";
+
 function OrganData(prop) {
+
+  const [diag, setDiag] = useState('');
+  const [newInfo, setnewInfo] = useState(' ' + '');
+  const [name, setName] = useState('');
+
+  const handleSubmit = async (event) => {
+    let id = prop._id
+    let info = diag;
+    event.preventDefault();
+    await updateOrgan(id, info)
+    setnewInfo(diag);
+    setDiag('');
+
+  }
+
   return (
     <div className="organDetails">
-      <img src={prop.img} />
+      <img src={prop.images} />
       <p>
         <span>Name:</span> {prop.name}
         <br />
@@ -9,11 +27,13 @@ function OrganData(prop) {
         <br />
         <span>Description:</span> {prop.description}
         <br />
-        <span>Diagnoses:</span> {prop.symptom}
+        <span>Diagnoses:</span> {prop.symptom + newInfo}
         <br />
-        <form>
+        <form onSubmit={handleSubmit}>
           <span> New Diagnoses: </span>
-          <input type="string"></input>
+          <input type="string"
+            value={diag}
+            onChange={(e) => setDiag(e.target.value)}></input>
           <button submit="">Submit</button>
         </form>
       </p>
